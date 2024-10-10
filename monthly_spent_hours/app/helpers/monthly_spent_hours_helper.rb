b3d49f83b0155aaca9dd6db0f8b037078bbc2163
y-kaneko <y-kaneko@milldea.com>
2024-10-10 15:28:43 +0900

    今月以外に作業をしていなければ、表示しないように変更

--- monthly_spent_hours/app/helpers/monthly_spent_hours_helper.rb
+++ monthly_spent_hours/app/helpers/monthly_spent_hours_helper.rb
@@ -1,13 +1,11 @@
 module MonthlySpentHoursHelper
   def issue_spent_hours_extended_details(spent_hours, total_spent_hours)
-    if total_spent_hours > 0
-      if total_spent_hours == spent_hours
-        l_hours_short(spent_hours)
-      else
-        s = spent_hours > 0 ? l_hours_short(spent_hours) : ""
-        s += " (#{l(:label_total)}: #{l_hours_short(total_spent_hours)})"
-        s.html_safe
-      end
+    if total_spent_hours == spent_hours
+      l_hours_short(spent_hours)
+    else
+      s = l_hours_short(spent_hours)
+      s += " (#{l(:label_total)}: #{l_hours_short(total_spent_hours)})"
+      s.html_safe
     end
   end  
 end
